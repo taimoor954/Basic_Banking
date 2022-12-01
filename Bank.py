36916cd928f295c78803e993a84eb3831caee685
taimoor954 <taimoormuhammad954@gmail.com>
2022-12-01 04:16:48 -0800

    Made a simple banking app for just to get more familiar with python syntax

--- Bank.py
+++ Bank.py
@@ -5,11 +5,14 @@ class Bank:
     def logTransaction(self, transactionString):
         print(transactionString)
         with open('transaction.txt', 'a') as file:
-            file.write(f"{transactionString}\n")
+            file.write(f"{transactionString} \t\t current balance is {self.balance}\n\n")
 
     def withDrawal(self, amount):
         try:
             amount = float(amount)
+            if amount > self.balance:
+                print('sorry but the amount youre asking is too much')
+                return
         except Exception as e:
            amount = 0 
         if amount:
@@ -27,9 +30,16 @@ class Bank:
              self.logTransaction(f"Transaction deposit is {amount}")
 
 
-
-account = Bank(50.50)
-account.deposit(50.50)
-print(account.balance)
-account.withDrawal(10)
-print(account.balance)
+account = Bank(0.0)
+while True:
+    action = input("What kind of action do you want to take? ")
+    if action in ['withdrawal', 'deposit']:
+        if action == 'withdrawal':
+            amount = input("How much do you want to with draw?")
+            account.withDrawal(amount)
+        elif action == 'deposit':
+            amount = input("How much do you want to deposit?")
+            account.deposit(amount)
+    else:
+        print("enter your action again")
+        
